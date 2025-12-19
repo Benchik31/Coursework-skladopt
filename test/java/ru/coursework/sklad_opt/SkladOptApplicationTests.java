cc0f1d47de8442420d036de61d5bf1304c899e6a
Benchik31 <127509428+Benchik31@users.noreply.github.com>
2025-12-19 22:09:24 +0300

    Add files via upload

---
+++ test/java/ru/coursework/sklad_opt/SkladOptApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coursework.sklad_opt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkladOptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
